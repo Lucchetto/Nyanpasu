23caadaf6bcb50c98c2af03dafac2f8ebf21061d
Lucchetto <lucchetto.tie@live.com>
2021-06-12 21:45:56 +0200

    Revert "LibraryFragment: use icons instead of text"
    This reverts commit 76946dee846e67a0b96df51ec51471e36db14f5d.
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- app/src/main/java/com/zhenxiang/nyaasi/fragment/LibraryFragment.kt
+++ app/src/main/java/com/zhenxiang/nyaasi/fragment/LibraryFragment.kt
@@ -35,10 +35,6 @@ class LibraryFragment : Fragment() {
         val viewPagerAdapter = LibraryPagerAdapter(childFragmentManager, viewPager.context)
         viewPager.adapter = viewPagerAdapter
         tabLayout.setupWithViewPager(viewPager)
-        val savedReleasesTab = tabLayout.getTabAt(0)
-        savedReleasesTab?.icon = viewPager.context.getDrawable(R.drawable.ic_outline_bookmarks_24)
-        val viewedReleasesTab = tabLayout.getTabAt(1)
-        viewedReleasesTab?.icon = viewPager.context.getDrawable(R.drawable.ic_outline_history_24)
         viewPager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener {
             var currentPage = 0
             override fun onPageScrolled(
@@ -92,7 +88,13 @@ class LibraryPagerAdapter(fragmentManager: FragmentManager, private val context:
         return 2
     }
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        return null
+    override fun getPageTitle(position: Int): CharSequence {
+        return when (position) {
+            0 -> context.getString(R.string.saved_fragment_bottom_nav)
+            1 -> context.getString(R.string.recently_viewed_fragment_bottom_nav)
+            else -> {
+                return context.getString(R.string.saved_fragment_bottom_nav)
+            }
+        }
     }
 }
