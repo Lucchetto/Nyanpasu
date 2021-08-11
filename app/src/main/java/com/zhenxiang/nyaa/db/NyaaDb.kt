905b2403c882dbb9e8d82f19eb659e5279b3c747
Lucchetto <lucchetto.tie@live.com>
2021-08-11 17:35:44 +0200

    Add migration schema from 1 to 2
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- app/src/main/java/com/zhenxiang/nyaa/db/NyaaDb.kt
+++ app/src/main/java/com/zhenxiang/nyaa/db/NyaaDb.kt
@@ -24,7 +24,24 @@ abstract class NyaaDb : RoomDatabase() {
 
         val MIGRATION_1_2 = object : Migration(1, 2) {
             override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL("ALTER TABLE nyaareleasepreview RENAME COLUMN id TO number;")
+                database.execSQL("""CREATE TABLE IF NOT EXISTS `new_nyaaReleasePreview` (`name` TEXT NOT NULL, `magnet` TEXT NOT NULL, `timestamp` INTEGER NOT NULL, `seeders` INTEGER NOT NULL, `leechers` INTEGER NOT NULL, `completed` INTEGER NOT NULL, `category` TEXT NOT NULL, `releaseSize` TEXT NOT NULL, `number` INTEGER NOT NULL, `dataSource` INTEGER NOT NULL, PRIMARY KEY(`number`, `dataSource`));
+                """)
+                database.execSQL("""INSERT INTO new_nyaaReleasePreview(dataSource, number, name, magnet, timestamp, seeders, leechers, completed, category, releaseSize) SELECT 0, id, name, magnet, timestamp, seeders, leechers, completed, category, releaseSize FROM nyaaReleasePreview;""")
+                database.execSQL("""DROP TABLE nyaaReleasePreview;""")
+                database.execSQL("""ALTER TABLE `new_nyaaReleasePreview` RENAME TO `nyaaReleasePreview`;""")
+                database.execSQL("""CREATE TABLE IF NOT EXISTS `new_nyaaReleaseDetails` (`user` TEXT, `hash` TEXT NOT NULL, `descriptionMarkdown` TEXT NOT NULL, `parent_number` INTEGER NOT NULL, `parent_dataSource` INTEGER NOT NULL, PRIMARY KEY(`parent_number`, `parent_dataSource`), FOREIGN KEY(`parent_number`, `parent_dataSource`) REFERENCES `NyaaReleasePreview`(`number`, `dataSource`) ON UPDATE CASCADE ON DELETE CASCADE )""")
+                database.execSQL("""INSERT INTO new_nyaaReleaseDetails(parent_dataSource, parent_number, user, hash, descriptionMarkdown) SELECT 0, parentId, user, hash, descriptionMarkdown FROM nyaaReleaseDetails;""")
+                database.execSQL("DROP TABLE nyaaReleaseDetails;")
+                database.execSQL("ALTER TABLE `new_nyaaReleaseDetails` RENAME TO `nyaaReleaseDetails`;")
+                database.execSQL("CREATE TABLE IF NOT EXISTS `new_viewedNyaaRelease` (`viewedTimestamp` INTEGER NOT NULL, `parent_number` INTEGER NOT NULL, `parent_dataSource` INTEGER NOT NULL, PRIMARY KEY(`parent_number`, `parent_dataSource`), FOREIGN KEY(`parent_number`, `parent_dataSource`) REFERENCES `NyaaReleasePreview`(`number`, `dataSource`) ON UPDATE CASCADE ON DELETE RESTRICT )")
+                database.execSQL("INSERT INTO new_viewedNyaaRelease(parent_dataSource, parent_number, viewedTimestamp) SELECT 0, releaseId, timestamp FROM viewedNyaaRelease")
+                database.execSQL("DROP TABLE viewedNyaaRelease;")
+                database.execSQL("ALTER TABLE `new_viewedNyaaRelease` RENAME TO `viewedNyaaRelease`;")
+                database.execSQL("CREATE TABLE IF NOT EXISTS `new_savedNyaaRelease` (`savedTimestamp` INTEGER NOT NULL, `parent_number` INTEGER NOT NULL, `parent_dataSource` INTEGER NOT NULL, PRIMARY KEY(`parent_number`, `parent_dataSource`), FOREIGN KEY(`parent_number`, `parent_dataSource`) REFERENCES `NyaaReleasePreview`(`number`, `dataSource`) ON UPDATE CASCADE ON DELETE RESTRICT )")
+                database.execSQL("INSERT INTO new_savedNyaaRelease(parent_dataSource, parent_number, savedTimestamp) SELECT 0, releaseId, timestamp FROM savedNyaaRelease")
+                database.execSQL("DROP TABLE savedNyaaRelease;")
+                database.execSQL("ALTER TABLE `new_savedNyaaRelease` RENAME TO `savedNyaaRelease`;")
+                database.execSQL("ALTER TABLE subscribedTracker ADD COLUMN `dataSource` INTEGER NOT NULL DEFAULT 0")
             }
         }
 
