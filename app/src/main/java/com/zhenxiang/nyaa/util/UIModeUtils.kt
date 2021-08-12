836e1a84757787f445dddca437971d329cb8c4d0
Lucchetto <lucchetto.tie@live.com>
2021-08-12 19:19:06 +0200

    Refactor UIModeUtils
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- app/src/main/java/com/zhenxiang/nyaa/util/UIModeUtils.kt
+++ app/src/main/java/com/zhenxiang/nyaa/util/UIModeUtils.kt
@@ -9,20 +9,18 @@ class UIModeUtils {
 
     companion object {
         fun updateUIMode(context: Context) {
-            setUIModeFromPref(
-                PreferenceManager.getDefaultSharedPreferences(context)
-                    .getString(context.getString(R.string.ui_mode_key), null)
-            )
+            AppCompatDelegate.setDefaultNightMode(getUiModeFromPref(context))
         }
 
-        fun setUIModeFromPref(value: String?) {
-            val uiMode = when (value) {
+        fun getUiModeFromPref(context: Context): Int {
+            val prefValue = PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(context.getString(R.string.ui_mode_key), null)
+            return when (prefValue) {
                 "0" -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
                 "1" -> AppCompatDelegate.MODE_NIGHT_NO
                 "2" -> AppCompatDelegate.MODE_NIGHT_YES
                 else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
             }
-            AppCompatDelegate.setDefaultNightMode(uiMode)
         }
     }
 }
