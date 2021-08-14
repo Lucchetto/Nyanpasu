f0c8befc05c39724af9d377fc1fc1e218e7b4c42
Lucchetto <lucchetto.tie@live.com>
2021-08-14 10:53:07 +0200

    Map all categories from sukebei.nyaa.si
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- app/src/main/java/com/zhenxiang/nyaa/api/SukebeiReleaseCategory.kt
+++ app/src/main/java/com/zhenxiang/nyaa/api/SukebeiReleaseCategory.kt
@@ -5,7 +5,15 @@ import com.zhenxiang.nyaa.R
 enum class SukebeiReleaseCategory(private val id: String, private val stringResId: Int): ReleaseCategory {
     // ALWAYS PUT DEFAULT CATEGORY AT TOP
     ALL("0_0", R.string.category_all),
-    ART("1_0", R.string.category_art);
+    ART("1_0", R.string.category_art),
+    ANIME("1_1", R.string.category_anime),
+    DOUJINSHI("1_2", R.string.category_doujinshi),
+    GAMES("1_3", R.string.category_games),
+    MANGA("1_4", R.string.category_manga),
+    PICTURES("1_5", R.string.category_pictures),
+    REAL_LIFE("2_0", R.string.category_real_life),
+    PHOTOBOOKS_PICTURES("2_1", R.string.category_photobooks_pictures),
+    VIDEOS("2_2", R.string.category_videos);
 
     override fun getDataSource(): ApiDataSource {
         return ApiDataSource.SUKEBEI_NYAA_SI
