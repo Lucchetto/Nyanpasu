cca911e5b31804dda46c6a2a154024712fe08b10
Lucchetto <lucchetto.tie@live.com>
2021-08-16 22:00:04 +0200

    Add nyanpasu sound effect
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- app/src/main/java/com/zhenxiang/nyaa/preference/FooterPreference.kt
+++ app/src/main/java/com/zhenxiang/nyaa/preference/FooterPreference.kt
@@ -1,7 +1,9 @@
 package com.zhenxiang.nyaa.preference
 
 import android.content.Context
+import android.media.MediaPlayer
 import android.util.AttributeSet
+import android.view.View
 import androidx.preference.Preference
 import androidx.preference.PreferenceViewHolder
 import com.zhenxiang.nyaa.R
@@ -12,12 +14,33 @@ class FooterPreference: Preference {
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int): super(context, attrs, defStyleAttr)
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int): super(context, attrs, defStyleAttr, defStyleRes)
 
+    private var mediaPlayer: MediaPlayer? = null
+
     init {
         layoutResource = R.layout.footer_preference
     }
 
     override fun onBindViewHolder(holder: PreferenceViewHolder) {
         super.onBindViewHolder(holder)
+        holder.itemView.findViewById<View>(R.id.renge).setOnClickListener { _ ->
+            playNyanpasu()
+        }
     }
 
+    private fun playNyanpasu() {
+        mediaPlayer?.let {
+            if (it.isPlaying) {
+                it.stop()
+                it.start()
+            }
+        } ?: run {
+            val newPlayer = MediaPlayer.create(context, R.raw.nyanpasu)
+            newPlayer.setOnCompletionListener {
+                it.release()
+                mediaPlayer = null
+            }
+            newPlayer.start()
+            mediaPlayer = newPlayer
+        }
+    }
 }
