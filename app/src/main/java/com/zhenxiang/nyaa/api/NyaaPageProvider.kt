fc3cf900c5111cb96db6c4e7d942dc0e024f9545
Lucchetto <lucchetto.tie@live.com>
2021-09-02 22:18:10 +0200

    Update jsoup to 1.14.2
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- app/src/main/java/com/zhenxiang/nyaa/api/NyaaPageProvider.kt
+++ app/src/main/java/com/zhenxiang/nyaa/api/NyaaPageProvider.kt
@@ -36,16 +36,16 @@ class NyaaPageProvider {
                 val doc: Document = Jsoup.connect(AppUtils.getReleasePageUrl(releaseId, useProxy)).get()
                 doc.outputSettings().prettyPrint(false)
 
-                val userName = doc.selectFirst("div.col-md-1:matches(Submitter:)").parent().select("a[href~=$userRegexString]").text()
-                val hash = doc.selectFirst("div.col-md-1:matches(Info hash:)").parent().select("kbd:matches(^(\\w{40})\$)").text()
-                val descriptionMarkdown = doc.getElementById("torrent-description").html()
+                val userName = doc.selectFirst("div.col-md-1:matches(Submitter:)")!!.parent()!!.select("a[href~=$userRegexString]").text()
+                val hash = doc.selectFirst("div.col-md-1:matches(Info hash:)")!!.parent()!!.select("kbd:matches(^(\\w{40})\$)").text()
+                val descriptionMarkdown = doc.getElementById("torrent-description")!!.html()
 
                 val comments = mutableListOf<ReleaseComment>()
                 val commentsContainer = doc.getElementById("collapse-comments")
                 commentsContainer?.select("*[id~=^com-\\d+\$]")?.forEach {
-                    val commentUsername = it.selectFirst("a[href~=$userRegexString]").text()
-                    val commentUrlImage = it.selectFirst("img").absUrl("src")
-                    val timestamp = it.selectFirst("*[data-timestamp~=^\\d+\$]").attr("data-timestamp").toLong()
+                    val commentUsername = it.selectFirst("a[href~=$userRegexString]")!!.text()
+                    val commentUrlImage = it.selectFirst("img")!!.absUrl("src")
+                    val timestamp = it.selectFirst("*[data-timestamp~=^\\d+\$]")!!.attr("data-timestamp").toLong()
                     val commentContent = Parser.unescapeEntities(it.select(".comment-content").html(), true)
                     comments.add(ReleaseComment(commentUsername, commentUrlImage, timestamp, commentContent))
                 }
@@ -90,19 +90,19 @@ class NyaaPageProvider {
             pageItems.forEach {
                 try {
                     // Get parent tr since we select element by a
-                    val parentRow = it.parent().parent()
+                    val parentRow = it.parent()!!.parent()!!
 
-                    val categoryHref =  parentRow.selectFirst("td > a[href~=$categoryIdRegexString]").attr("href")
+                    val categoryHref =  parentRow.selectFirst("td > a[href~=$categoryIdRegexString]")!!.attr("href")
                     val category = DataSourceSpecs.getCategoryFromId(dataSource, categoryIdRegex.find(categoryHref)!!.groupValues[0])
 
                     val number = releaseIdRegex.find(it.attr("href"))!!.groupValues[0].toInt()
                     val title = it.attr("title")
-                    val magnetLink = parentRow.selectFirst("a[href~=^magnet:\\?xt=urn:[a-z0-9]+:[a-z0-9]{32,40}&dn=.+&tr=.+\$]").attr("href")
-                    val timestamp = parentRow.selectFirst("*[data-timestamp~=^\\d+\$]").attr("data-timestamp").toString().toLong()
+                    val magnetLink = parentRow.selectFirst("a[href~=^magnet:\\?xt=urn:[a-z0-9]+:[a-z0-9]{32,40}&dn=.+&tr=.+\$]")!!.attr("href")
+                    val timestamp = parentRow.selectFirst("*[data-timestamp~=^\\d+\$]")!!.attr("data-timestamp").toString().toLong()
                     val seeders = parentRow.select("td:nth-child(6)").text().toInt()
                     val leechers = parentRow.select("td:nth-child(7)").text().toInt()
                     val completed = parentRow.select("td:nth-child(8)").text().toInt()
-                    val releaseSize = parentRow.selectFirst("td:matches(^\\d*\\.?\\d* [a-zA-Z]+\$)").text()
+                    val releaseSize = parentRow.selectFirst("td:matches(^\\d*\\.?\\d* [a-zA-Z]+\$)")!!.text()
 
                     val nyaaItem = NyaaReleasePreview(number, DataSourceSpecs(dataSource, category), title, magnetLink, timestamp, seeders, leechers, completed, releaseSize)
                     foundReleases.add(nyaaItem)
